1c2559544f55c886b132ac985eaa82d6493eea56
pseudopulse <113800509+pseudopulse@users.noreply.github.com>
2024-08-29 16:10:02 -0500

    Initial commit

---
+++ ModName/Plugin.cs
@@ -0,0 +1,32 @@
+using BepInEx;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Reflection;
+using RoR2.UI;
+using MonoMod.Cil;
+using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ModName {
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class ModName : BaseUnityPlugin {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "ModAuthorName";
+        public const string PluginName = "ModName";
+        public const string PluginVersion = "1.0.0";
+
+        public static BepInEx.Logging.ManualLogSource ModLogger;
+
+        public void Awake() {
+            // set logger
+            ModLogger = Logger;
+        }
+    }
+}
